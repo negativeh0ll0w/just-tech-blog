c1beac78d4f79f73c4b9f93b5351ce0ee4abd746
Sarah King <sarahking573@gmail.com>
2022-07-09 16:04:42 -0400

    fix sequelize error

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
-const sequelize = require("./config/connection");
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
